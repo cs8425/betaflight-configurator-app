7d553bdf62f5d5dc3f476210c5ccb5b0f6a88e89
cs8425 <cs8425@gmail.com>
2017-02-10 21:58:48 +0800

    ugly but work

---
+++ build.sh
@@ -0,0 +1,3 @@
+time cordova build android --release -- --keystore=apk-test-key.keystore --storePassword=123456 --alias=test --password=123456
+cp platforms/android/build/outputs/apk/android-release.apk .
+
